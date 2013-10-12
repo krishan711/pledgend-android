12d8ba423ce129cc9231cf7ed381e481c22012b3
Grant Kemp <grant@kempnet.com>
2013-10-12 17:31:55 +0100

    updated constants

--- Pledgend/src/main/java/com/pledgend/Constants.java
+++ Pledgend/src/main/java/com/pledgend/Constants.java
@@ -4,4 +4,9 @@ package com.pledgend;
  * Created by gkemp on 10/12/13.
  */
 public class Constants {
+// test credentials
+    String paypal_Sandbox_ApiKey="";
+    String paypal_endpoint="api.sandbox.paypal.com";
+    String paypal_clientId="ARS4WxAdVdBsNqMVwc_Lp-qYkbNhEksD3b2lj5pAHL0Gv2sQepXfzSP7IBmA";
+    String paypal_clientSecret="EOcZ7BAbuXg63l4bwhjkW0hYAixtUd72yo73sYQ9ggrpPUfTvq_pe0SB_2D6";
 }
